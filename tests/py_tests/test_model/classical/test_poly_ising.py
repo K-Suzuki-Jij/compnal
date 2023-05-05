ed443c495cdd78d03e2bb254128598c5df9adf49
KSuzuki <eixecosxpisinx@gmail.com>
2023-05-06 00:15:48 +0900

    Update poly ising tests

--- tests/py_tests/test_model/classical/test_poly_ising.py
+++ tests/py_tests/test_model/classical/test_poly_ising.py
@@ -6,9 +6,18 @@ from compnal.lattice import Chain, Square, Cubic, InfiniteRange
 
 def test_poly_ising_chain():
     chain = Chain(system_size=4, boundary_condition="OBC")
-    poly_ising = PolyIsing(lattice=chain, interaction={0: -1.0, 1: +2.0, 2: -3.0, 3: +4.0})
+    poly_ising = PolyIsing(lattice=chain, interaction={0: -1.0, 1: +2.0, 2: -3.0, 3: +4.0}, spin_magnitude=1)
     assert poly_ising.get_interaction() == {0: -1.0, 1: +2.0, 2: -3.0, 3: +4.0}
     assert poly_ising.get_degree() == 3
+    assert poly_ising.get_spin_magnitude() == {0: 1, 1: 1, 2: 1, 3: 1}
+    poly_ising.set_spin_magnitude(2, 0)
+    assert poly_ising.get_spin_magnitude() == {0: 2, 1: 1, 2: 1, 3: 1}
+
+    with pytest.raises(ValueError):
+        poly_ising.set_spin_magnitude(1.4999, 0)
+    
+    with pytest.raises(ValueError):
+        poly_ising.set_spin_magnitude(1.5, 10)
 
     with pytest.raises(ValueError):
         poly_ising.calculate_energy([-1, +1])
@@ -40,6 +49,23 @@ def test_poly_ising_square():
     poly_ising = PolyIsing(lattice=square, interaction={0: -1.0, 1: +2.0, 2: -3.0, 3: +4.0})
     assert poly_ising.get_interaction() == {0: -1.0, 1: +2.0, 2: -3.0, 3: +4.0}
     assert poly_ising.get_degree() == 3
+    assert poly_ising.get_spin_magnitude() == {
+        (0,0): 0.5, (1,0): 0.5, (2,0): 0.5, (3,0): 0.5,
+        (0,1): 0.5, (1,1): 0.5, (2,1): 0.5, (3,1): 0.5,
+        (0,2): 0.5, (1,2): 0.5, (2,2): 0.5, (3,2): 0.5
+    }
+    poly_ising.set_spin_magnitude(2, (0,1))
+    assert poly_ising.get_spin_magnitude() == {
+        (0,0): 0.5, (1,0): 0.5, (2,0): 0.5, (3,0): 0.5,
+        (0,1): 2.0, (1,1): 0.5, (2,1): 0.5, (3,1): 0.5,
+        (0,2): 0.5, (1,2): 0.5, (2,2): 0.5, (3,2): 0.5
+    }
+
+    with pytest.raises(ValueError):
+        poly_ising.set_spin_magnitude(1.4999, (0,0))
+    
+    with pytest.raises(ValueError):
+        poly_ising.set_spin_magnitude(1.5, (10,0))
 
     with pytest.raises(ValueError):
         poly_ising.calculate_energy(np.reshape([-1,+1,-1,+1,+1,+1], (2, 3)))
@@ -69,9 +95,32 @@ def test_poly_ising_square():
 
 def test_poly_ising_cubic():
     cubic = Cubic(x_size=4, y_size=3, z_size=2, boundary_condition="OBC")
-    poly_ising = PolyIsing(lattice=cubic, interaction={0: -1.0, 1: +2.0, 2: -3.0, 3: +4.0})
+    poly_ising = PolyIsing(lattice=cubic, interaction={0: -1.0, 1: +2.0, 2: -3.0, 3: +4.0}, spin_magnitude=3)
     assert poly_ising.get_interaction() == {0: -1.0, 1: +2.0, 2: -3.0, 3: +4.0}
     assert poly_ising.get_degree() == 3
+    assert poly_ising.get_spin_magnitude() == {
+        (0,0,0): 3, (1,0,0): 3, (2,0,0): 3, (3,0,0): 3,
+        (0,1,0): 3, (1,1,0): 3, (2,1,0): 3, (3,1,0): 3,
+        (0,2,0): 3, (1,2,0): 3, (2,2,0): 3, (3,2,0): 3,
+        (0,0,1): 3, (1,0,1): 3, (2,0,1): 3, (3,0,1): 3,
+        (0,1,1): 3, (1,1,1): 3, (2,1,1): 3, (3,1,1): 3,
+        (0,2,1): 3, (1,2,1): 3, (2,2,1): 3, (3,2,1): 3,
+    }
+    poly_ising.set_spin_magnitude(2, (0,0,1))
+    assert poly_ising.get_spin_magnitude() == {
+        (0,0,0): 3, (1,0,0): 3, (2,0,0): 3, (3,0,0): 3,
+        (0,1,0): 3, (1,1,0): 3, (2,1,0): 3, (3,1,0): 3,
+        (0,2,0): 3, (1,2,0): 3, (2,2,0): 3, (3,2,0): 3,
+        (0,0,1): 2, (1,0,1): 3, (2,0,1): 3, (3,0,1): 3,
+        (0,1,1): 3, (1,1,1): 3, (2,1,1): 3, (3,1,1): 3,
+        (0,2,1): 3, (1,2,1): 3, (2,2,1): 3, (3,2,1): 3,
+    }
+
+    with pytest.raises(ValueError):
+        poly_ising.set_spin_magnitude(1.4999, (0,0,0))
+    
+    with pytest.raises(ValueError):
+        poly_ising.set_spin_magnitude(1.5, (10,0,0))
 
     with pytest.raises(ValueError):
         poly_ising.calculate_energy(np.reshape([-1,+1,-1,+1,+1,+1,-1,+1,-1,+1,+1,+1], (2, 3, 2)))
@@ -101,9 +150,18 @@ def test_poly_ising_cubic():
 
 def test_poly_ising_infinite_range():
     infinite_range = InfiniteRange(system_size=4)
-    poly_ising = PolyIsing(lattice=infinite_range, interaction={0: -1.0, 1: +2.0, 2: -3.0, 3: +4.0})
+    poly_ising = PolyIsing(lattice=infinite_range, interaction={0: -1.0, 1: +2.0, 2: -3.0, 3: +4.0}, spin_magnitude=1)
     assert poly_ising.get_interaction() == {0: -1.0, 1: +2.0, 2: -3.0, 3: +4.0}
     assert poly_ising.get_degree() == 3
+    assert poly_ising.get_spin_magnitude() == {0: 1, 1: 1, 2: 1, 3: 1}
+    poly_ising.set_spin_magnitude(2, 0)
+    assert poly_ising.get_spin_magnitude() == {0: 2, 1: 1, 2: 1, 3: 1}
+
+    with pytest.raises(ValueError):
+        poly_ising.set_spin_magnitude(1.4999, 0)
+    
+    with pytest.raises(ValueError):
+        poly_ising.set_spin_magnitude(1.5, 10)
 
     with pytest.raises(ValueError):
         poly_ising.calculate_energy([+1,+1,+1,+1,+1])

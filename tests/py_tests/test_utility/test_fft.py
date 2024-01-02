cb68a1a35bb94ac4e90057dc912fd24297519403
KSuzuki <eixecosxpisinx@gmail.com>
2024-01-03 02:04:33 +0900

    Add power option

--- tests/py_tests/test_utility/test_fft.py
+++ tests/py_tests/test_utility/test_fft.py
@@ -17,6 +17,18 @@ def test_fft_magnitude():
     compnal_fft = calculate_fft_magnitude_list(array, norm="backward", num_threads=2)
     assert np.allclose(numpy_fft, compnal_fft)
 
+    numpy_fft = np.array([np.abs(np.fft.fft(array[i], norm="ortho")) for i in range(3)])
+    compnal_fft = calculate_fft_magnitude_list(array, norm="ortho", power=2, num_threads=2)
+    assert np.allclose(numpy_fft**2, compnal_fft)
+
+    numpy_fft = np.array([np.abs(np.fft.fft(array[i], norm="forward")) for i in range(3)])
+    compnal_fft = calculate_fft_magnitude_list(array, norm="forward", power=2, num_threads=2)
+    assert np.allclose(numpy_fft**2, compnal_fft)
+
+    numpy_fft = np.array([np.abs(np.fft.fft(array[i], norm="backward")) for i in range(3)])
+    compnal_fft = calculate_fft_magnitude_list(array, norm="backward", power=2, num_threads=2)
+    assert np.allclose(numpy_fft**2, compnal_fft)
+
     with pytest.raises(ValueError):
         calculate_fft_magnitude_list(np.random.rand(3, 4, 2), num_threads=2)
 
@@ -33,6 +45,18 @@ def test_fft2_magnitude():
     numpy_fft = np.array([np.abs(np.fft.fft2(array[i], norm="backward")) for i in range(3)])
     compnal_fft = calculate_fft2_magnitude_list(array, norm="backward", num_threads=2)
     assert np.allclose(numpy_fft, compnal_fft)
+
+    numpy_fft = np.array([np.abs(np.fft.fft2(array[i], norm="ortho")) for i in range(3)])
+    compnal_fft = calculate_fft2_magnitude_list(array, norm="ortho", power=2, num_threads=2)
+    assert np.allclose(numpy_fft**2, compnal_fft)
+
+    numpy_fft = np.array([np.abs(np.fft.fft2(array[i], norm="forward")) for i in range(3)])
+    compnal_fft = calculate_fft2_magnitude_list(array, norm="forward", power=2, num_threads=2)
+    assert np.allclose(numpy_fft**2, compnal_fft)
+
+    numpy_fft = np.array([np.abs(np.fft.fft2(array[i], norm="backward")) for i in range(3)])
+    compnal_fft = calculate_fft2_magnitude_list(array, norm="backward", power=2, num_threads=2)
+    assert np.allclose(numpy_fft**2, compnal_fft)
 
     with pytest.raises(ValueError):
         calculate_fft2_magnitude_list(np.random.rand(3, 4), num_threads=2)

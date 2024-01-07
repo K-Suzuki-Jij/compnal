61626d1d1ac0d8ae78c20bb9f91d4608afb94d32
KSuzuki <eixecosxpisinx@gmail.com>
2024-01-07 16:57:01 +0900

    Fix

---
+++ dependencies/FetchPybind11.cmake
@@ -0,0 +1,10 @@
+include(FetchContent)
+
+message(STATUS "Downloading pybind11...")
+FetchContent_Declare(
+  pybind11
+  GIT_REPOSITORY https://github.com/pybind/pybind11.git
+  GIT_TAG        v2.11.1
+)
+FetchContent_MakeAvailable(pybind11)
+message(STATUS "pybind11 download complete.")

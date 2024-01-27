77798813d3a71a854918dcd9d17017b06618c598
KSuzuki <eixecosxpisinx@gmail.com>
2024-01-27 23:55:18 +0900

    Fix bug for update func. for infinite_range

--- tests/cpp_tests/solver/classical_monte_carlo/system/test_system_poly_ising_infinite_range.hpp
+++ tests/cpp_tests/solver/classical_monte_carlo/system/test_system_poly_ising_infinite_range.hpp
@@ -27,7 +27,7 @@
 namespace compnal {
 namespace test {
 
-TEST(SolverClassicalMonteCarloSystem, PolyIsingOnInfiniteRange) {
+TEST(SolverClassicalMonteCarloSystem, PolyIsingOnInfiniteRangeSpinHalf) {
    using InfiniteRange = lattice::InfiniteRange;
    using PolyIsing = model::classical::PolynomialIsing<InfiniteRange>;
    
@@ -89,6 +89,81 @@ TEST(SolverClassicalMonteCarloSystem, PolyIsingOnInfiniteRange) {
          initial_state[i] = -0.5;
          initial_state[0] = -0.5;
          initial_state[4] = -0.5;
+      }
+   }
+}
+
+TEST(SolverClassicalMonteCarloSystem, PolyIsingOnInfiniteRangeSpinOne) {
+   using InfiniteRange = lattice::InfiniteRange;
+   using PolyIsing = model::classical::PolynomialIsing<InfiniteRange>;
+   
+   const std::int32_t system_size = 10;
+   std::vector<std::unordered_map<std::int32_t, double>> interaction_set = {
+      {{1, -1.5}, {2, +1.0}},
+      {{1, -1.5}, {2, +0.5}, {3, -1.0}},
+      {{1, -1.5}, {2, +0.5}, {3, +1.0}, {4, -2.0}},
+      {{1, -1.5}, {2, +0.5}, {3, +1.0}, {4, -2.0}, {5, +3.0}},
+      {{1, -1.5}, {2, +0.5}, {3, +1.0}, {4, -2.0}, {5, +3.0}, {6, -10}},
+      {{1, -1.5}, {2, +0.5}, {3, +1.0}, {4, -2.0}, {5, +3.0}, {6, -10}, {7, -3.5}},
+      {{1, -1.5}, {2, +0.5}, {3, +1.0}, {4, -2.0}, {5, +3.0}, {6, -10}, {7, -3.5}, {8, -1.5}},
+   };
+   
+   Eigen::Vector<double, Eigen::Dynamic> initial_state(system_size);
+   for (std::int32_t i = 0; i < system_size; ++i) {
+      initial_state[i] = -1;
+   }
+   
+   const auto check_include = [](const std::int32_t val, const std::vector<std::int32_t> &list) {
+      for (const auto &it: list) {
+         if (val == it) {
+            return true;
+         }
+      }
+      return false;
+   };
+   
+   for (const auto &interaction: interaction_set) {
+      InfiniteRange infinite_range{system_size};
+      PolyIsing poly_ising{infinite_range, interaction, 1};
+      
+      const std::int32_t seed = 0;
+      
+      solver::classical_monte_carlo::System<PolyIsing, std::mt19937> system{poly_ising, seed};
+      system.SetSampleByValue(initial_state);
+      
+      for (std::int32_t i = 0; i < system_size; ++i) {
+         EXPECT_DOUBLE_EQ(system.ExtractSample()(i), -1);
+         EXPECT_TRUE(check_include(system.GenerateCandidateState(i), {1, 2}));
+      }
+      
+      EXPECT_EQ(system.GetSystemSize(), system_size);
+      
+      for (std::int32_t i = 1; i < system_size; ++i) {
+         initial_state[i] = 0;
+         EXPECT_DOUBLE_EQ(system.GetEnergy(), poly_ising.CalculateEnergy(system.ExtractSample()));
+         EXPECT_DOUBLE_EQ(system.GetEnergyDifference(i, 1),
+                          poly_ising.CalculateEnergy(initial_state) -
+                          poly_ising.CalculateEnergy(system.ExtractSample()));
+         
+         system.Flip(0, 1);
+         EXPECT_DOUBLE_EQ(system.GetEnergy(), poly_ising.CalculateEnergy(system.ExtractSample()));
+         initial_state[0] = 0;
+         EXPECT_DOUBLE_EQ(system.GetEnergyDifference(i, 1),
+                          poly_ising.CalculateEnergy(initial_state) -
+                          poly_ising.CalculateEnergy(system.ExtractSample()));
+         
+         system.Flip(4, 1);
+         EXPECT_DOUBLE_EQ(system.GetEnergy(), poly_ising.CalculateEnergy(system.ExtractSample()));
+         initial_state[4] = 0;
+         EXPECT_DOUBLE_EQ(system.GetEnergyDifference(i, 1),
+                          poly_ising.CalculateEnergy(initial_state) -
+                          poly_ising.CalculateEnergy(system.ExtractSample()));
+         
+         system.Flip(0, 0);
+         system.Flip(4, 0);
+         initial_state[i] = -1;
+         initial_state[0] = -1;
+         initial_state[4] = -1;
       }
    }
 }

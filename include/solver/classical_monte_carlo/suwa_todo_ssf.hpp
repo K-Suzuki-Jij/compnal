79699e39636f74a266dbc4f026cc40dff91aa23c
KSuzuki <eixecosxpisinx@gmail.com>
2024-03-01 23:49:15 +0900

    Update

--- include/solver/classical_monte_carlo/suwa_todo_ssf.hpp
+++ include/solver/classical_monte_carlo/suwa_todo_ssf.hpp
@@ -25,6 +25,65 @@ namespace compnal {
 namespace solver {
 namespace classical_monte_carlo {
 
+template<class SystemType, typename RandType>
+class SuwaTodoUpdater {
+  
+public:
+   SuwaTodoUpdater(const std::int32_t max_num_state,
+                   const SystemType &system,
+                   const double beta,
+                   const typename RandType::result_type seed):
+   system_(system), beta_(beta), random_number_engine_(seed), dist_real(0, 1) {
+      weight_list_.resize(max_num_state);
+      sum_weight_list_.resize(max_num_state + 1);
+   }
+   
+   std::int32_t GetNewState(const std::int32_t index) {
+      return GetNewStateAny(index);
+   }
+   
+private:
+   const SystemType &system_;
+   const double beta_;
+   RandType random_number_engine_;
+   std::uniform_real_distribution<double> dist_real;
+   std::vector<double> weight_list_;
+   std::vector<double> sum_weight_list_;
+   
+   std::int32_t GetNewStateAny(const std::int32_t index) {
+      const std::int32_t num_state = system_.GetNumState(index);
+      const std::int32_t max_weight_state = system_.GetMaxBoltzmannWeightStateNumber(index);
+      
+      weight_list_[0] = std::exp(-beta_*system_.GetEnergyDifference(index, max_weight_state));
+      sum_weight_list_[1] = weight_list_[0];
+      
+      for (std::int32_t state = 1; state < num_state; ++state) {
+         if (state == max_weight_state) {
+            weight_list_[state] = std::exp(-beta_*system_.GetEnergyDifference(index, 0));
+         }
+         else {
+            weight_list_[state] = std::exp(-beta_*system_.GetEnergyDifference(index, state));
+         }
+         sum_weight_list_[state + 1] = sum_weight_list_[state] + weight_list_[state];
+      }
+      sum_weight_list_[0] = sum_weight_list_[num_state];
+      
+      const std::int32_t temp = system_.GetStateNumber(index);
+      const std::int32_t now_state = (temp == 0) ? max_weight_state : ((temp == max_weight_state) ? 0 : temp);
+      double prob_sum = 0.0;
+      const double dist = dist_real(random_number_engine_);
+      for (std::int32_t j = 0; j < num_state; ++j) {
+         const double d_ij = sum_weight_list_[now_state + 1] - sum_weight_list_[j] + sum_weight_list_[1];
+         prob_sum += std::max(0.0, std::min({d_ij, 1.0 + weight_list_[j] - d_ij, 1.0, weight_list_[j]}));
+         if (dist < prob_sum) {
+            return (j == max_weight_state) ? 0 : ((j == 0) ? max_weight_state : j);
+         }
+      }
+      return num_state - 1;
+   }
+
+};
+
 //! @brief Run classical monte carlo simulation using Metropolis method.
 //! @tparam SystemType System class.
 //! @tparam RandType Random number engine class.
@@ -42,81 +101,28 @@ void SuwaTodoSSF(SystemType *system,
    
    const std::int32_t system_size = system->GetSystemSize();
    
-   // Set random number engine
-   RandType random_number_engine(seed);
-   std::uniform_real_distribution<double> dist_real(0, 1);
-   
-   const auto get_new_state = [](const std::vector<double> &prob_list,
-                                 const std::int32_t num_state,
-                                 const double dist_real) {
-      double prob_sum = 0.0;
-      for (std::int32_t state = 0; state < num_state; state++) {
-         if (dist_real < prob_list[state] + prob_sum) {
-            return state;
-         }
-         prob_sum += prob_list[state];
-      }
-      return num_state - 1;
-   };
-
    // Find Max number of state
    std::int32_t max_num_state = 0;
    for (std::int32_t i = 0; i < system_size; i++) {
       max_num_state = std::max(max_num_state, system->GetNumState(i));
    }
    
-   std::vector<double> dW(max_num_state);
-   std::vector<double> S(max_num_state + 1);
-   //std::vector<std::int32_t> indices(max_num_state);
-   //std::iota(indices.begin(), indices.end(), 0);
-
-   
    if (spin_selector == SpinSelectionMethod::RANDOM) {
+      RandType random_number_engine(seed);
       std::uniform_int_distribution<std::int32_t> dist_system_size(0, system_size - 1);
+      auto updater = SuwaTodoUpdater<SystemType, RandType>(max_num_state, *system, beta, random_number_engine());
       for (std::int32_t sweep_count = 0; sweep_count < num_sweeps; sweep_count++) {
          for (std::int32_t i = 0; i < system_size; i++) {
-//            const std::int32_t index = dist_system_size(random_number_engine);
-//            const std::int32_t num_state = system->GetNumState(index);
-//            for (std::int32_t state = 0; state < num_state; ++state) {
-//               dW[state] = std::exp(-beta*system->GetEnergyDifference(index, state));
-//            }    
-//            const auto &prob_list = suwa_todo(dW, system->GetStateNumber(index), S);
-//            std::int32_t new_state = get_new_state(prob_list, dist_real(random_number_engine));
-//            system->Flip(index, new_state);
+            const std::int32_t index = dist_system_size(random_number_engine);
+            system->Flip(index, updater.GetNewState(index));
          }
       }
    }
    else if (spin_selector == SpinSelectionMethod::SEQUENTIAL) {
+      auto updater = SuwaTodoUpdater<SystemType, RandType>(max_num_state, *system, beta, seed);
       for (std::int32_t sweep_count = 0; sweep_count < num_sweeps; sweep_count++) {
          for (std::int32_t i = 0; i < system_size; i++) {
-            const std::int32_t num_state = system->GetNumState(i);
-            const std::int32_t max_weight_state = system->GetMaxBoltzmannWeightStateNumber(i);
-            dW[0] = std::exp(-beta*system->GetEnergyDifference(i, max_weight_state));
-            S[1] = dW[0];
-            for (std::int32_t state = 1; state < num_state; ++state) {
-               if (state == max_weight_state) {
-                  dW[state] = std::exp(-beta*system->GetEnergyDifference(i, 0));
-               }
-               else {
-                  dW[state] = std::exp(-beta*system->GetEnergyDifference(i, state));
-               }
-               S[state + 1] = S[state] + dW[state];
-            }
-            S[0] = S[num_state];
-            
-            const std::int32_t now_state = (system->GetStateNumber(i) == 0) ? max_weight_state : ((system->GetStateNumber(i) == max_weight_state) ? 0 : system->GetStateNumber(i));
-            std::int32_t new_state = num_state - 1;
-            double prob_sum = 0.0;
-            const double dist = dist_real(random_number_engine);
-            for (std::int32_t j = 0; j < num_state; ++j) {
-               const double d_ij = S[now_state + 1] - S[j] + dW[0];
-               prob_sum += std::max(0.0, std::min({d_ij, dW[now_state] + dW[j] - d_ij, dW[now_state], dW[j]}));
-               if (dist < prob_sum) {
-                  new_state = (j == max_weight_state) ? 0 : ((j == 0) ? max_weight_state : j);
-                  break;
-               }
-            }
-            system->Flip(i, new_state);
+            system->Flip(i, updater.GetNewState(i));
          }
       }
    }
